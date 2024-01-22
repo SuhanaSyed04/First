cb69c92988b688e90c9184cefb02eb834d1e4009
Suhana Syed <104555183+SuhanaSyed04@users.noreply.github.com>
2024-01-22 23:16:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("Hello World");
+var a = 10;
+console.log(typeof a);
+
+var marks = 98;
+if(marks>80){
+    console.log("Outstanding");
+}
+else if(marks>=60){
+    console.log("Good");
+}
+else{
+    console.log("Fail")
+}
